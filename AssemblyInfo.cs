c5da089d1213288a7de27b8086fa8a3a1ed72806
Emre Yurtseven <emreyurtseven18@gmail.com>
2026-02-07 15:57:06 +0100

    chore: remove assembly versioning attributes from AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,9 +1,4 @@
-using System.Reflection;
 using System.Windows;
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
